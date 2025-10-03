614a4129edf902138869bc015c55c0b365fd989f
Walter Sjöblom <walter.sjoblom@skola.taby.se>
2025-10-03 09:11:33 +0200

    programmering

---
+++ hello.py
@@ -0,0 +1,21 @@
+
+Gissaträtt = False
+import random 
+while not Gissaträtt:
+
+
+    x = random.randint (1,100)
+
+    första = int(input("skriv en siffra"))
+
+    if första == x:
+        print("Rätt")
+        Gissaträtt = True
+    if första < x:
+        print("högre")
+    else:
+        print("lägre")
+
+
+
+lista = [10,20,30,40]
---
+++ traning.py
@@ -0,0 +1,10 @@
+a=3
+b=7
+c=4
+d=11
+
+def hej(g,h,i,j):
+    summi = g+h+i-j
+    return summi 
+print(hej(a, b, c, d))
+
